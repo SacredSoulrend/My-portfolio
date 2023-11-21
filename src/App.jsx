e000f9f9e7945e7601303da9201922ad9fb8e687
Son Giang <sgiangt@gmail.com>
2023-11-20 18:34:23 -0700

    fixed typo in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
-import Hero from './Components/Hero';
+import Hero from './components/Hero';
 import AboutMe from './pages/AboutMe';
 import Portfolio from './pages/Portfolio';
 import Contact from './pages/Contact';
